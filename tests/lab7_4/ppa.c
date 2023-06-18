a4b27aa661e6020e60af9398db9204f495b0dfca
quanshr <2514295417@qq.com>
2023-06-18 11:46:59 +0800

    dd

--- tests/lab7_4/ppa.c
+++ tests/lab7_4/ppa.c
@@ -1,35 +1,34 @@
 #include <lib.h>
-struct sigset_t set2;
-int cnt=0;
-void handler(int num){
-cnt++;
-if(cnt>10){
-return ;
+
+int global = 0;
+void handler(int num) {
+    debugf("Reach handler, now the signum is %d!\n", num);
+    int ans = 0;
+    for (int i = 0; i < 10000000; i++) {
+        ans += i;
+    }
+    debugf("end handler, now the signum is %d!\n", num);
+    global = 1;
 }
-debugf("cnt:%d HANDLER:%x %d\n",cnt,syscall_getenvid(),num);
-}
+
+#define TEST_NUM 2
 int main(int argc, char **argv) {
-struct sigset_t set;
-set.sig[0]=set.sig[1]=0;
-struct sigaction act;
-act.sa_mask=set;
-act.sa_handler=handler;
-sigaction(10,&act,NULL);
-int father=syscall_getenvid();
-int ret = fork();
-debugf("father:%d child:%d\n",syscall_getenvid(),ret);
-if (ret != 0) {
-for(int i=0;i<10;i++){
-kill(ret,10);
+    sigset_t set;
+    sigemptyset(&set);
+    struct sigaction sig;
+    sig.sa_handler = handler;
+    sig.sa_mask = set;
+    panic_on(sigaction(TEST_NUM, &sig, NULL));
+    sigaddset(&set, TEST_NUM);
+    panic_on(sigprocmask(0, &set, NULL));
+    kill(0, TEST_NUM);
+    kill(0, TEST_NUM);
+    int ans = 0;
+    for (int i = 0; i < 10000000; i++) {
+        ans += i;
+    }
+    panic_on(sigprocmask(1, &set, NULL));
+    debugf("global = %d.\n", global);
+    return 0;
 }
-while(cnt!=10);
-debugf("Father passed!\n");
-} else {
-for(int i=0;i<10;i++){
-kill(father,10);
-}
-while(cnt!=10);
-debugf("Child passed!\n");
-}
-return 0;
-}
+
